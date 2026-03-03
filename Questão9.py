21e371894be635af34120c06dbb8ccd0d67aeb4a
Eimilly__S2 <emillysantosaaraujo@gmail.com>
2026-03-03 18:42:28 -0300

    Add files via upload
    9.                  Receba os 2 números inteiros. Calcule e mostre a soma dos quadrados.

---
+++ Questão9.py
@@ -0,0 +1,14 @@
+#Declarar
+
+N1 : int = 0
+N2 : int = 0
+Soma : int = 0
+
+#Inicio
+
+N1 = int (input("Digite o 1° Número: "))
+N2 = int(input("Digite o 2° Número: "))
+Soma = (N1*N1 + N2*N2)
+print("A soma dos quadrados é: ",Soma)
+
+#Fim
